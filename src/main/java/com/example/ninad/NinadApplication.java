b24758d30958ce78a01abbb253f712a72ac195c2
NinadPurkar <purkar22ninad@gmail.com>
2025-12-03 11:58:08 +0530

    Initial commit

---
+++ src/main/java/com/example/ninad/NinadApplication.java
@@ -0,0 +1,13 @@
+package com.example.ninad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ninad/NinadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ninad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
